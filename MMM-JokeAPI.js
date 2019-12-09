f4f1fe6e09e4d27859bd5444fd906ccaa74a8a3e
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2019-12-09 22:10:03 +0100

    Intial commit

---
+++ MMM-JokeAPI.js
@@ -0,0 +1,58 @@
+Module.register("jokes", {
+    defaults: {
+        category: "Programming",
+        fetchInterval: 10 * 1000
+    },
+    getStyles() {
+        return [
+            this.file('style.css')
+        ]
+    },
+    joke: null,
+    notificationReceived(notification, payload, sender) {
+        if (notification === 'MODULE_DOM_CREATED') {
+            this.getJoke();
+            setInterval(() => {
+                this.getJoke()
+            }, this.config.fetchInterval);
+        }
+    },
+    getDom() {
+        const wrapper = document.createElement("div");
+
+        if(this.joke === null) return wrapper;
+
+        this.setupHTMLStructure(wrapper);
+
+        return wrapper;
+    },
+    setupHTMLStructure(wrapper) {
+        if (this.joke.type === 'single') {
+
+            const joke = document.createElement("h1");
+            joke.className = "bright medium light fadeInJoke";
+            joke.innerHTML = this.joke.joke;
+            wrapper.appendChild(joke);
+
+        } else if (this.joke.type === 'twopart') {
+
+            const setup = document.createElement("h1");
+            setup.className = "bright medium light no-wrap fadeInJoke";
+            setup.innerHTML = this.joke.setup;
+            wrapper.appendChild(setup);
+
+            const punchline = document.createElement("h2");
+            punchline.className = "bright small light fadeInPunchline";
+            punchline.innerHTML = this.joke.delivery;
+            wrapper.appendChild(punchline);
+        }
+    },
+    getJoke() {
+        fetch(`https://sv443.net/jokeapi/category/${this.config.category}`).then((response) => {
+            response.json().then((joke) => {
+                this.joke = joke;
+                this.updateDom();
+            });
+        });
+    }
+});
---
+++ README.md
@@ -0,0 +1,31 @@
+# MagicMirror² JokeAPI module
+This is a module for `MagicMirror²`; the purpose of the module is to show you jokes from the JokeAPI.
+
+## Installation
+1.  Clone this repository into your MagicMirror `modules` folder.
+2.  Edit your configuration file under `config/config.js` with the following configuration.
+```
+{
+    module: 'MMM-JokeAPI',
+    position: 'middle_center',
+    config: {
+        category: "Programming"
+    }
+}
+```
+
+*The following categories are available*
+1.  Programming
+2.  Miscellaneous
+3.  Dark
+4.  Any
+
+
+### Preview
+![Screenshot](screenshot.png)
+
+
+
+#### Credits
+MagicMirror²:   [MagicMirror²](https://github.com/MichMich/MagicMirror)   
+JokeAPI:    [JokeAPI](https://sv443.net/jokeapi)
